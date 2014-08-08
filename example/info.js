ddaf14e694e8f8f6f0f1725f1e021bfe88c5a26b
Tamara Chahine <chahine.tamara@abc.net.au>
2014-08-08 15:35:42 +1000

    Allow vimeo URLs

--- example/info.js
+++ example/info.js
@@ -1,16 +1,22 @@
 var ytdl = require('..');
-var url = 'http://www.youtube.com/watch?v=0RUvealeXZ0';
+var url = 'http://vimeo.com/93491987';
 
-ytdl.getInfo(url, function(err, info) {
-  if (err) throw err;
+ytdl.getInfo(url, function(err, data) {
+    if (err) throw err;
 
-  console.log('id:', info.id);
-  console.log('title:', info.title);
-  console.log('url:', info.url);
-  console.log('thumbnail:', info.thumbnail);
-  console.log('description:', info.description);
-  console.log('filename:', info.filename);
-  console.log('itag:', info.itag);
-  console.log('resolution:', info.resolution);
+    var format = data[data.length - 1].split(' - ');
+
+    var video = {
+      title       : data[0],
+      id          : data[1],
+      url         : data[2],
+      thumbnail   : data[3],
+      description : data.slice(4, data.length - 2).join('\n'),
+      filename    : data[data.length - 2],
+      itag        : parseInt(format[0], 10),
+      resolution  : format[1]
+    };
+
+    console.log(err, data, video);
 }
 );
